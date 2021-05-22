225e26c95bf9a52ab65ff7e5857f527d77c2b5c6
teefisher2k20 <teefisher314@gmail.com>
2021-05-22 10:57:17 -0500

    updated gradle

--- src/main/java/org/launchcode/projectliftoff/controllers/CandidateController.java
+++ src/main/java/org/launchcode/projectliftoff/controllers/CandidateController.java
@@ -5,10 +5,10 @@ import org.launchcode.projectliftoff.models.CandidateRegistration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("candidate")
